69913d87b23d63c7ed843f9620f347a943c50aea
sachinArraykart <sachin.jha.i@arraykart.com>
2021-12-16 20:18:39 +0530

    facebook login

--- app/src/main/java/com/example/arraykart/MainActivity.java
+++ app/src/main/java/com/example/arraykart/MainActivity.java
@@ -3,8 +3,16 @@ package com.example.arraykart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +28,22 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         }, 3000);
+
+        printKeyHash();
+    }
+
+    private void printKeyHash() {
+        try{
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for(Signature signature:info.signatures){
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KEY_HASH", Base64.encodeToString(md.digest(),Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/arraykart/SignUP.java
+++ app/src/main/java/com/example/arraykart/SignUP.java
@@ -1,24 +1,80 @@
 package com.example.arraykart;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.login.Login;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
 public class SignUP extends AppCompatActivity {
+
+    CallbackManager callbackManager;
+
     private ImageView imageView;
     private TextView signintv;
-    private AppCompatButton signupbtn;
+    private Button signupbtn;
+    private EditText signUpUserName,signUpUserEmail,signUpUserPassword,signUpUserPassw0rdConform;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_sign_up);
+
+        callbackManager =CallbackManager.Factory.create();
+
+        signUpUserName = findViewById(R.id.signUpUserName);
+        signUpUserEmail = findViewById(R.id.signUpUserEmail);
+        signUpUserPassword = findViewById(R.id.signUpUserPassword);
+        signUpUserPassw0rdConform = findViewById(R.id.signUpUserPasswordConform);
+
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                handleFacebookToken(loginResult.getAccessToken());
+
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(),"cancel",Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onError(@NonNull FacebookException e) {
+                Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
         try{
             imageView = findViewById(R.id.imageView6);
             imageView.setOnClickListener(new View.OnClickListener() {
@@ -49,8 +105,7 @@ public class SignUP extends AppCompatActivity {
             signupbtn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    finish();
-                    Toast.makeText(getApplicationContext(), "Signed Up!", Toast.LENGTH_SHORT).show();
+
                 }
             });
         }catch (Exception e){
@@ -58,4 +113,51 @@ public class SignUP extends AppCompatActivity {
         }
 
     }
+
+    public void logIn(View view){
+        LoginManager.getInstance().logInWithReadPermissions(this,
+                Arrays.asList("email")
+        );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    GraphRequest request1;
+
+    private void handleFacebookToken(AccessToken token){
+        Log.d("tokenfb",token.getToken());
+        Profile profile = Profile.getCurrentProfile();
+        if(profile!=null){
+            String facebook_id= profile.getId();
+            String UName = profile.getName();
+            String email = profile.getLastName();
+
+            signUpUserName.setText(UName);
+            signUpUserEmail.setText(email);
+
+            request1 = GraphRequest.newMeRequest(token, new GraphRequest.GraphJSONObjectCallback() {
+                @Override
+                public void onCompleted(@Nullable JSONObject jsonObject, @Nullable GraphResponse graphResponse) {
+                    try {
+                        String email_id= jsonObject.getString("email");
+                        String gender = jsonObject.getString("gender");
+                        String profile_name = jsonObject.getString("name");
+                        String fb_id = jsonObject.getString("id"); ///we will use this for logout
+
+                        Log.d("email_id","email_id");
+                        Log.d("gender","gender");
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            });
+            request1.executeAsync();
+        }
+    }
+
 }
