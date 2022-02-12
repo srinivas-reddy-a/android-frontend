ed3ba586456a047edd2c9f933ae71a4caf6e6122
srinivas-reddy-a <it@arraykart.com>
2022-02-12 18:52:18 +0530

    check

--- app/src/main/java/com/example/arraykart/ui/HelpCenter/HelpCenterFragment.java
+++ app/src/main/java/com/example/arraykart/ui/HelpCenter/HelpCenterFragment.java
@@ -1,5 +1,6 @@
 package com.example.arraykart.ui.HelpCenter;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.arraykart.HelpCenterActivity.HelpCenterActivity;
 import com.example.arraykart.R;
 import com.example.arraykart.databinding.FragmentHelpCenterBinding;
 
@@ -21,6 +23,8 @@ public class HelpCenterFragment extends Fragment {
         // Inflate the layout for this fragment
         binding=FragmentHelpCenterBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        Intent intent = new Intent(getContext(),HelpCenterActivity.class);
+        startActivity(intent);
 
         return root;
     }
